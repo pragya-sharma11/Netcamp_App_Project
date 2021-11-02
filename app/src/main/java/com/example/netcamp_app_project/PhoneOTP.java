599e706d343290a758bdc94d55f3c265da01385e
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-02 14:45:57 +0530

    make functionalities for phone OTP

--- app/src/main/java/com/example/netcamp_app_project/PhoneOTP.java
+++ app/src/main/java/com/example/netcamp_app_project/PhoneOTP.java
@@ -2,13 +2,42 @@ package com.example.netcamp_app_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PhoneOTP extends AppCompatActivity {
-
+    Button b1, b2, b3;
+    EditText e1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_o_t_p);
+        e1 = findViewById(R.id.editText4);
+        b1 = findViewById(R.id.button7);
+        b2 = findViewById(R.id.button8);
+        b3 = findViewById(R.id.button9);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PhoneOTP.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
