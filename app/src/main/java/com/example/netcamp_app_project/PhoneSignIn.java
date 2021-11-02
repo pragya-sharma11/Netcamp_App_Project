e6ee4aefa7ad48b7b5f812eeefa695d9d12f3023
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-02 14:48:31 +0530

    commit

--- app/src/main/java/com/example/netcamp_app_project/PhoneSignIn.java
+++ app/src/main/java/com/example/netcamp_app_project/PhoneSignIn.java
@@ -8,10 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.hbb20.CountryCodePicker;
+
 public class PhoneSignIn extends AppCompatActivity {
     Button b1, b2, b3;
     EditText e1;
-
+    CountryCodePicker ccp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +22,14 @@ public class PhoneSignIn extends AppCompatActivity {
         b1 = findViewById(R.id.button);
         b2 = findViewById(R.id.button5);
         b3 = findViewById(R.id.button6);
+        ccp = findViewById(R.id.ccp);
+        ccp.registerCarrierNumberEditText(e1);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i = new Intent(PhoneSignIn.this, PhoneOTP.class);
+                i.putExtra("phone",ccp.getFullNumberWithPlus().trim());
+                startActivity(i);
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
