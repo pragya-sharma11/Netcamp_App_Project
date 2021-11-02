38485643079337fef9a0b19d487555b8bccaf7a9
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-02 14:43:31 +0530

    make functionalities for phone signin

--- app/src/main/java/com/example/netcamp_app_project/PhoneSignIn.java
+++ app/src/main/java/com/example/netcamp_app_project/PhoneSignIn.java
@@ -2,13 +2,43 @@ package com.example.netcamp_app_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PhoneSignIn extends AppCompatActivity {
+    Button b1, b2, b3;
+    EditText e1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_sign_in);
+        e1 = findViewById(R.id.editText3);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button5);
+        b3 = findViewById(R.id.button6);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PhoneSignIn.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
