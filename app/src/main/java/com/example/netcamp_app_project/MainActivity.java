3dcfe04d14d0165d99026ed8fe900e85d3fc2f45
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-02 14:31:25 +0530

    make functionality for phone button in main activity.

--- app/src/main/java/com/example/netcamp_app_project/MainActivity.java
+++ app/src/main/java/com/example/netcamp_app_project/MainActivity.java
@@ -63,5 +63,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PhoneSignIn.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
