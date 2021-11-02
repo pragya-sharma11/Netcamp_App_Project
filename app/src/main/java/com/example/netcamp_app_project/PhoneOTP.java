6052f3835998ebecbafb03997a906c2be466be83
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-02 13:37:43 +0530

    Make layout for login with phone

---
+++ app/src/main/java/com/example/netcamp_app_project/PhoneOTP.java
@@ -0,0 +1,14 @@
+package com.example.netcamp_app_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_o_t_p);
+    }
+}
