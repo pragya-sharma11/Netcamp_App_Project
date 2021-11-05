04b3a4cd3ded6d833b5bb7835a1f32536556edb5
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-06 00:34:44 +0530

    declare and initialize view variables.

--- app/src/main/java/com/example/netcamp_app_project/Camera.java
+++ app/src/main/java/com/example/netcamp_app_project/Camera.java
@@ -3,12 +3,25 @@ package com.example.netcamp_app_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Camera extends AppCompatActivity {
-
+    ImageView i1;
+    Button b1, b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        i1 = findViewById(R.id.Cameraimage1);
+        b1 = findViewById(R.id.click);
+        b2 = findViewById(R.id.Cameraback);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
