ebdc48ee9de29fac1e68e7a404e2ef4d8f739aa9
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-07 02:03:38 +0530

    make functionality for music on/off.

--- app/src/main/java/com/example/netcamp_app_project/Music.java
+++ app/src/main/java/com/example/netcamp_app_project/Music.java
@@ -2,13 +2,31 @@ package com.example.netcamp_app_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Music extends AppCompatActivity {
-
+    ImageButton i;
+    Boolean music = false;
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+        i = findViewById(R.id.musicButton);
+        mp = MediaPlayer.create(this, R.raw.y);
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!music){
+                    mp.start();
+                }else{
+                    mp.pause();
+                }
+                music = !music;
+            }
+        });
     }
 }
