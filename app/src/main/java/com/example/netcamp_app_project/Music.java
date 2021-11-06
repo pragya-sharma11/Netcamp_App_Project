3ece95c3aced99521beff4c580f971e8b0ec7260
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-07 02:06:55 +0530

    make functionality for back button in music activity.

--- app/src/main/java/com/example/netcamp_app_project/Music.java
+++ app/src/main/java/com/example/netcamp_app_project/Music.java
@@ -2,21 +2,25 @@ package com.example.netcamp_app_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Music extends AppCompatActivity {
     ImageButton i;
     Boolean music = false;
     MediaPlayer mp;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
         i = findViewById(R.id.musicButton);
         mp = MediaPlayer.create(this, R.raw.y);
+        b = findViewById(R.id.musicBack);
         i.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +30,14 @@ public class Music extends AppCompatActivity {
                     mp.pause();
                 }
                 music = !music;
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Music.this, PhoneScreen.class);
+                startActivity(i);
+                finish();
             }
         });
     }
