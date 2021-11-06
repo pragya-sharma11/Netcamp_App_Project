27a601ee304d6745ae627004c586a33816c8e0fe
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-07 02:10:25 +0530

    create PhoneCall activity

---
+++ app/src/main/java/com/example/netcamp_app_project/PhoneCall.java
@@ -0,0 +1,14 @@
+package com.example.netcamp_app_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneCall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_call);
+    }
+}
