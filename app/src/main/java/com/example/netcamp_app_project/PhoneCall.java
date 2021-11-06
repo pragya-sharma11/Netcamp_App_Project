39dbc6ccf709b3f05a7692189b9b6dd3c8c215e1
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-07 02:29:39 +0530

    make function for call in PhoneCall activity

--- app/src/main/java/com/example/netcamp_app_project/PhoneCall.java
+++ app/src/main/java/com/example/netcamp_app_project/PhoneCall.java
@@ -2,13 +2,30 @@ package com.example.netcamp_app_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PhoneCall extends AppCompatActivity {
-
+    EditText e1;
+    ImageButton i1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_call);
+        e1 = findViewById(R.id.PhoneCallEditText);
+        i1 = findViewById(R.id.phonebutton);
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = e1.getText().toString();
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:"+s1));
+                startActivity(i);
+            }
+        });
     }
 }
