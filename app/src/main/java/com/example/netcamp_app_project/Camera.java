62343bddac08120391e2d08eb3de2da8cb02f0ef
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-07 00:47:03 +0530

    camera activity completed.

--- app/src/main/java/com/example/netcamp_app_project/Camera.java
+++ app/src/main/java/com/example/netcamp_app_project/Camera.java
@@ -1,11 +1,16 @@
 package com.example.netcamp_app_project;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Camera extends AppCompatActivity {
     ImageView i1;
@@ -20,8 +25,20 @@ public class Camera extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
             }
         });
+    }
+    @MainThread
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 100){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            i1.setImageBitmap(bitmap);
+        }
+        else{
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+        }
     }
 }
