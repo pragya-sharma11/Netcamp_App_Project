e73f7034053dbcdf28f852d9bfd5f636f7cc6989
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-07 02:36:16 +0530

    make function for back in PhoneCall activity

--- app/src/main/java/com/example/netcamp_app_project/PhoneCall.java
+++ app/src/main/java/com/example/netcamp_app_project/PhoneCall.java
@@ -6,18 +6,29 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class PhoneCall extends AppCompatActivity {
     EditText e1;
     ImageButton i1;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_call);
         e1 = findViewById(R.id.PhoneCallEditText);
         i1 = findViewById(R.id.phonebutton);
+        b = findViewById(R.id.phoneBack);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PhoneCall.this, PhoneScreen.class);
+                startActivity(i);
+                finish();
+            }
+        });
         i1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
