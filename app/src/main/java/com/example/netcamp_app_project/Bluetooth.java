946b394faee884617d12bc9aaa49e397ad156094
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-08 23:23:33 +0530

    make functionality to on/off bluetooth

--- app/src/main/java/com/example/netcamp_app_project/Bluetooth.java
+++ app/src/main/java/com/example/netcamp_app_project/Bluetooth.java
@@ -2,13 +2,32 @@ package com.example.netcamp_app_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Bluetooth extends AppCompatActivity {
-
+    Button b1, b2;
+    BluetoothAdapter b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
+        b1 = findViewById(R.id.BluetoothOn);
+        b2 = findViewById(R.id.BluetoothOff);
+        b = BluetoothAdapter.getDefaultAdapter();
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.enable();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.disable();
+            }
+        });
     }
 }
