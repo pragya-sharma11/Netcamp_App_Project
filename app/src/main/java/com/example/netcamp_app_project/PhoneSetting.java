5df422ac9d949b691d98dd07576ae99b25f51aa5
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-08 23:57:46 +0530

    make functionality for vibrator and torch in PhoneSetting

--- app/src/main/java/com/example/netcamp_app_project/PhoneSetting.java
+++ app/src/main/java/com/example/netcamp_app_project/PhoneSetting.java
@@ -5,22 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class PhoneSetting extends AppCompatActivity {
     ImageButton i1, i2, i3, i4;
-    boolean blt = false, wifi = false, flash = false, vibrate = false;
+    boolean blt = false, wifi = false, flash = false;
     Button back;
     BluetoothAdapter b;
     WifiManager wf;
     CameraManager cm;
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +34,7 @@ public class PhoneSetting extends AppCompatActivity {
         b = BluetoothAdapter.getDefaultAdapter();
         wf = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         cm = (CameraManager) getApplicationContext().getSystemService(CAMERA_SERVICE);
+        //SDK is important for it sdk>=24
         i1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,18 +62,18 @@ public class PhoneSetting extends AppCompatActivity {
         i3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(PhoneSetting.this, Torch.class);
-                startActivity(i);
-                finish();
+                if(!flash){
+                    flashOn();
+                }else{
+                    flashOff();
+                }
             }
         });
 
         i4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(PhoneSetting.this, Vibrate.class);
-                startActivity(i);
-                finish();
+                ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(2000);
             }
         });
 
@@ -83,5 +85,23 @@ public class PhoneSetting extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void flashOn(){
+        try {
+            String id  = cm.getCameraIdList()[0];
+            cm.setTorchMode(id,true);
+            flash=true;
+        }catch (CameraAccessException e){
+
+        }
+    }
+    public void flashOff(){
+        try {
+            String id  = cm.getCameraIdList()[0];
+            cm.setTorchMode(id,false);
+            flash=false;
+        }catch (CameraAccessException e){
+
+        }
     }
 }
